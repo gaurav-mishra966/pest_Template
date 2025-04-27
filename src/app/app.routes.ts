4b61925210f4afcc758cfecaa3936ddb550b70ab
gaurav-mishra966 <gaurav.mishra@magicedtech.com>
2025-04-27 22:22:44 +0530

    SecondCommit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ServicesComponent } from './components/services/services.component';
 import { ContactusComponent } from './components/contactus/contactus.component';
+import { FreeQuotesComponent } from './components/free-quotes/free-quotes.component';
 
 export const routes: Routes = [
   { path: '', component: ServicesComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactusComponent },
+  { path: 'quote', component: FreeQuotesComponent },
   { path: '**', redirectTo: '' },
 ];
