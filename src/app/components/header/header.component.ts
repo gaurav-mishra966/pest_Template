46986d9433cbfb934f106aa00f0a972bf5e50180
gaurav-mishra966 <gaurav.mishra@magicedtech.com>
2025-04-27 20:59:10 +0530

    Update header.component.ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  menuActive = true;
+  menuActive = false;
   toggleMenu() {
     this.menuActive = !this.menuActive;
   }
